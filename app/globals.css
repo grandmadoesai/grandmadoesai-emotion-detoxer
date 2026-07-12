b3177a13b8a31576997dd7e81b7137571c9bc095
grandmadoesai <rivadiva95@gmail.com>
2026-07-11 23:37:05 -0700

    Create globals.css

---
+++ app/globals.css
@@ -0,0 +1,19 @@
+import './globals.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Objective Reality Matching System',
+  description: 'PNW FSBO Emotion Detoxer',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
